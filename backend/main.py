ded4bced5dabe6e329ecba57f18e9ec109d6d89e
William Chesser <bchesser@unc.edu>
2024-09-01 13:57:23 -0400

    Update backend for production

--- backend/main.py
+++ backend/main.py
@@ -78,4 +78,4 @@ async def validate_word(request: WordValidationRequest):
     return {"valid": is_valid}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
