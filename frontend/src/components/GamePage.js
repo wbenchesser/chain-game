88adb7c559d68346f7c5fcc9dc4100e6834ae0ac
William Chesser <bchesser@unc.edu>
2024-09-02 13:48:47 -0400

    Bigfixes and tweaks

--- frontend/src/components/GamePage.js
+++ frontend/src/components/GamePage.js
@@ -62,7 +62,7 @@ const GamePage = () => {
       setLoading(true);
 
       try {
-        const response = await fetch('http://127.0.0.1:8000/validate/', {
+        const response = await fetch('/api/validate/', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
